01ef46b2006df08be994b4a9d837fcb986e4d276
garciam <markel.garciad@gmail.com>
2018-05-31 16:02:40 +0200

    disabled tests as they will not work in other computers out of the box

--- tests/test_extract_and_join.py
+++ tests/test_extract_and_join.py
@@ -17,12 +17,11 @@ def test_ofile():
     opt.paxis = True
     opt.OFILE = "/tmp/test_cf.nc"
     opt.quiet = False
-    #opt.vtable = '/home/users/garciam/git/WRFtoolbox_github/WRFToolbox/wrfncxnj/wrfncxnj/wrfncxnj.table'
-    opt.geofile = "/predictia/Projects/metromadrid/domains/metromad_lakes/geo_em.d03.nc"
+    opt.geofile = "path_to_geofile"
     opt.time_units = "hours since 1950-01-01 00:00:00"
     opt.oformat = "NETCDF4_CLASSIC"
     opt.ftimes = "2016070412,2016070418"
-    args = ["/home/users/garciam/pruebas/wrf2cf/wrfout.nc", ]
+    args = ["path_to_wrfout", ]
     extract_and_join = ExtractAndJoin(opt, args)
     extract_and_join.run()
 
@@ -33,16 +32,15 @@ def test_output_pattern():
     opt.splitvars = True
     opt.splitlevs = True
     opt.OUTPUT_PATTERN = "/tmp/[varcf]_[varwrf]_[level]_[firsttime]_[lasttime]_test.nc"
-    opt.vtable = '/home/users/garciam/git/WRFtoolbox_github/WRFToolbox/wrfncxnj/wrfncxnj/wrfncxnj.table'
-    opt.geofile = "/predictia/Projects/metromadrid/domains/metromad_lakes/geo_em.d03.nc"
+    opt.geofile = "path_to_geofile"
     opt.time_units = "hours since 1950-01-01 00:00:00"
     opt.ftimes = "2016070412,2016070418"
     opt.oformat = "NETCDF4_CLASSIC"
-    args = ["/home/users/garciam/pruebas/wrf2cf/wrfout.nc", ]
+    args = ["path_to_wrfout", ]
     extract_and_join = ExtractAndJoin(opt, args)
     extract_and_join.run()
 
 
 if __name__ == "__main__":
-    test_ofile()
+    #test_ofile()
     #test_output_pattern()
