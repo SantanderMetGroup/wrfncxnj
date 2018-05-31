402f7ce4bff55941e5a9ea95c7e67b65d7112f11
garciam <markel.garciad@gmail.com>
2018-05-31 15:20:42 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+requires = ['numpy', 'netCDF4']
+
+setup(
+    name='wrfncxnj',
+    platforms=['GNU/Linux'],
+    version='0.1',
+    description="WRF netCDF Extract and Join",
+    install_requires=requires,
+    packages=['wrfncxnj', ],
+    license='GNU General Public License',
+    scripts=['bin/wrfncxnj']
+)
