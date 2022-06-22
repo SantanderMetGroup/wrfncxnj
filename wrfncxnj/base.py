SantanderMetGroup/wrfncxnj

commit dd96da99ad56a746f0abadfe53ca16c61d48c6b8

Add possibility to read lons and lats from fullfile ...
Fix mentions to older versions of the --split-variables flag
in the help text.

wrfncxnj/base.py
@@ -276,13 +276,17 @@ def create_bare_curvilinear_CF_from_wrfnc(
             oncm[:] = single_level
         onc.sync()
     #
-    # Lat-lons (from geo_em file if provided)
+    # Lat-lons (from geo_em or full file if provided)
     #
     if opt.geofile:
         incgeo = ncdf.Dataset(opt.geofile, 'r')
-        incgeo = ncdf.Dataset(opt.geofile, 'r')
         lats = incgeo.variables["XLAT_M"][0]
         lons = incgeo.variables["XLONG_M"][0]
+        incgeo.close()
+    if opt.fullfile:
+        incgeo = ncdf.Dataset(opt.fullfile, 'r')
+        lats = incgeo.variables["XLAT"][0]
+        lons = incgeo.variables["XLONG"][0]
         incgeo.close()
     else:
         lats = inc.variables["XLAT"][0]
@@ -534,6 +538,9 @@ units: %(units)s
                 else:
                     levels = incfull.variables["ZNU"][0]
                 incfull.close()
+            else:
+                raise RuntimeError(
+                    "Error, no eta levels found in the input files")
         #
         # Height levels in meters
         #

wrfncxnj/options.py
@@ -101,7 +101,7 @@ def get_options():
     parser.add_option(
         "--output-pattern", dest="OUTPUT_PATTERN",
         metavar="[varcf]_[varwrf]_[level]_[firsttime]_[lasttime]_experiment.nc",
-        help="Output pattern to use if the option --split-in-variables is "
+        help="Output pattern to use if the option --split-variables is "
              "activated. Patterns recognized are currently of the "
              "form '[varcf]_[varwrf]_[firsttime]_[lasttime]_experiment.nc. "
              "Firsttime and lasttime are replaced by datetimes of the "
@@ -143,9 +143,9 @@ def get_options():
              " meters. They must be in .2f format."
     )
     parser.add_option(
-        "--output-format", dest="oformat", default="NETCDF3_CLASSIC",
+        "--output-format", dest="oformat", default="NETCDF4_CLASSIC",
         help="Format of the output files. Available possibilities: "
-             "NETCDF4_CLASSIC, NETCDF3 (default). If using NETCDF4_CLASSIC, "
+             "NETCDF4_CLASSIC (default), NETCDF3. If using NETCDF4_CLASSIC, "
              "the deflate level is 4 by default."
     )
     parser.add_option(
@@ -178,8 +178,8 @@ def check_options_consistency(opt):
 
     if not opt.splitvars and opt.OUTPUT_PATTERN:
         raise AssertionError("An output pattern is needed to use "
-                             "--split-in-variables. Please set it with the "
+                             "--split-variables. Please set it with the "
                              "--output-pattern option")
     if not opt.splitvars and opt.selected_plevs:
-        raise AssertionError("--split-in-vars and -sel-plevs must be used at "
+        raise AssertionError("--split-variables and -sel-plevs must be used at "
                              "the same time")

commit 43b5c61a826724877912b26272a6b6d9ad29f4f0

Fix XLAT XLONG reading
Bug was introduced in dd96da9

wrfncxnj/base.py
@@ -283,7 +283,7 @@ def create_bare_curvilinear_CF_from_wrfnc(
         lats = incgeo.variables["XLAT_M"][0]
         lons = incgeo.variables["XLONG_M"][0]
         incgeo.close()
-    if opt.fullfile:
+    elif opt.fullfile:
         incgeo = ncdf.Dataset(opt.fullfile, 'r')
         lats = incgeo.variables["XLAT"][0]
         lons = incgeo.variables["XLONG"][0]